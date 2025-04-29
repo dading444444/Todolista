2008e0949028b8623ef38eab7b09ba4b9c74933e
dading444444 <m.aldi.gimain.con@gmail.com>
2025-04-29 21:39:39 +0700

    menghubungkan dengan firebese

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ import {
   orderBy,
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZZzmOl8wJa2420wAoSMfDVTEKxj6tEqk",
+  authDomain: "aldi-eb45b.firebaseapp.com",
+  projectId: "aldi-eb45b",
+  storageBucket: "aldi-eb45b.firebasestorage.app",
+  messagingSenderId: "74072339126",
+  appId: "1:74072339126:web:37a27a2f60108fbe9f6c17",
+  measurementId: "G-SXG0MRRXS6"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
