1315718822b6116958426dfd03ff86f88af45431
dading444444 <m.aldi.gimain.con@gmail.com>
2025-04-29 21:37:01 +0700

    menginfor modul firebese

---
+++ main.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
